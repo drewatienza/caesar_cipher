3232276ae14155cb20d0b79e143428acef15e7c3
drewatienza <atienza.drew@gmail.com>
2021-02-07 16:47:40 -0800

    added decryption functionality

--- cipher.py
+++ cipher.py
@@ -1,7 +1,12 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+encode_decode = True
+
+while encode_decode:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    if direction == 'encode' or direction == 'decode':
+        encode_decode = False
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
@@ -17,4 +22,18 @@ def encrypt(text_input, shift_amount):
     print(f'The encoded text is {encrypted_word}.')
 
 
-encrypt(text, shift)
+def decrypt(text_input, shift_amount):
+    decrypted_word = ''
+    for letter in text_input:
+        shift_index = alphabet.index(letter) - shift_amount
+        if shift_index < 0:
+            decrypted_word += alphabet[shift_index + 26]
+        else:
+            decrypted_word += alphabet[shift_index]
+    print(f'The decrypted text is {decrypted_word}.')
+
+
+if direction == 'encode':
+    encrypt(text, shift)
+else:
+    decrypt(text, shift)
