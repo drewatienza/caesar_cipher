49778b5b4f7c7867b3d73a12d0ab5db5c69e648f
drewatienza <atienza.drew@gmail.com>
2021-02-08 05:31:44 -0800

    finished project

--- cipher.py
+++ cipher.py
@@ -1,14 +1,9 @@
+from art import logo
+
+print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-encode_decode = True
-
-while encode_decode:
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-    if direction == 'encode' or direction == 'decode':
-        encode_decode = False
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 
 def caesar(text_input, shift_amount, cipher_direction):
@@ -26,7 +21,28 @@ def caesar(text_input, shift_amount, cipher_direction):
                 end_text += alphabet[shift_index + 26]
             else:
                 end_text += alphabet[shift_index]
-    print(f'The encoded text is {end_text}.')
+    print(f'The {cipher_direction}d text is {end_text}.')
 
 
-caesar(text, shift, direction)
+encode_decode = True
+continue_cipher = True
+
+while continue_cipher:
+    while encode_decode:
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        if direction == 'encode' or direction == 'decode':
+            encode_decode = False
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    if shift > 25:
+        shift = shift % 26
+
+    caesar(text, shift, direction)
+
+    restart = input('Do you want to go again?  Type "yes" or "no".\n')
+    if restart == 'no':
+        continue_cipher = False
+        print('Goodbye')
+
+
